c0854000636f3d64ff862a0cba5f6af3031afae8
Gonéri Le Bouder <goneri@lebouder.net>
2019-06-03 13:05:36 -0400

    do not build the qcow2 files

--- build-all.sh
+++ build-all.sh
@@ -41,9 +41,10 @@ echo '/dev/gpt/rootfs   /       ufs     rw      1       1
     echo 'sendmail_enable="NONE"' >> ${WORK_DIR}/etc/rc.conf
     makefs -B little -o label=freebsd_root ./ufs ${WORK_DIR}
     mkimg -s gpt -b ${WORK_DIR}/boot/pmbr -p efi:=${WORK_DIR}/boot/boot1.efifat -p freebsd-boot:=${WORK_DIR}/boot/gptboot -p freebsd-swap/swapfs::1G -p freebsd-ufs/rootfs:=./ufs -o freebsd-${VERSION}.raw
-    qemu-img convert -c -f raw -O qcow2 freebsd-${VERSION}.raw freebsd-${VERSION}.qcow2
+    #qemu-img convert -c -f raw -O qcow2 freebsd-${VERSION}.raw freebsd-${VERSION}.qcow2
     chflags -R noschg ${WORK_DIR}
-    rm -r ${WORK_DIR} freebsd-${VERSION}.raw
+    rm -r ${WORK_DIR}
+    #rm -r ${WORK_DIR} freebsd-${VERSION}.raw
 }
 
 build 12.0
