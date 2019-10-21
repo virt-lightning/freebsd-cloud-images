79cec2597f2ba688adcbc3180f80dd069ee012b6
Gonéri Le Bouder <goneri@lebouder.net>
2019-10-21 12:44:12 -0400

    avoid ftp-archive.freebsd.org, too slow

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ set -eux
 
 function build {
     VERSION=$1
-    BASE_URL="ftp://ftp-archive.freebsd.org/pub/FreeBSD-Archive/old-releases/amd64/${VERSION}-RELEASE"
+    BASE_URL="ftp://ftp.freebsd.org/pub/FreeBSD/releases/amd64/${VERSION}-RELEASE"
     WORK_DIR="/root/work_dir_${VERSION}"
     mkdir ${WORK_DIR}
     curl -L ${BASE_URL}/base.txz | tar vxf - -C ${WORK_DIR}
